4d374c2c2ed7a3c0b0f6859958c903378b4bd205
Shrey Gupta <shrey4317@gmail.com>
2024-04-08 17:03:19 +0530

    Add project files.

---
+++ StudentManagement.api/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.api.Model;
+
+namespace StudentManagement.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private static List<Student> _students = new List<Student>()
+        {
+            new Student(){StudentId=1,FirstName="Shrey",LastName="Gupta",Age=23,Email="shrey@gmail.com"},
+            new Student(){StudentId=2,FirstName="Harsh",LastName="Gupta",Age=20,Email="harsh@gmail.com"},
+            new Student(){StudentId=3,FirstName="Pandit",LastName="pandey",Age=23,Email="pandit@gmail.com"},
+            new Student(){StudentId=4,FirstName="Asu",LastName="Srivastav",Age=27,Email="asu@gmail.com"},
+            new Student(){StudentId=5,FirstName="Harshit",LastName="Singh",Age=23,Email="harshit@gmail.com"},
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_students);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Student student)
+        {
+            _students.Add(student);
+            return Ok(_students);
+        }
+    }
+}
---
+++ StudentManagement.api/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManagement.api.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public int Age { get; set;}
+
+        public string Email { get; set;}
+    }
+}
